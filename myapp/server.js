f4809b2729bd2fefea0a43018c2b05fcc6cf40c6
adebiyiebun <wvbw15@durham.ac.uk>
2020-12-26 23:28:01 +0000

    js

--- myapp/server.js
+++ myapp/server.js
@@ -1,1 +1,8 @@
-
+const express = require('express')
+const app = express();
+app.arguments(express.static('client'));
+let movies = ['Inside Out', 'Up', 'Toy Story'];
+app.get('/list', function(req, resp){
+    resp.send(movies);
+});
+app.listen(8090);
