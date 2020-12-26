8f28dedd6800330a4886fd10691325b552d7afe3
adebiyiebun <wvbw15@durham.ac.uk>
2020-12-26 22:58:10 +0000

    dk

--- myapp/server.js
+++ myapp/server.js
@@ -1,14 +1,1 @@
 
-
-var express = require('express')
-var app = express()
-
-app.get('/', function(req, resp){
-  resp.send('Hello')
-})
-
-app.listen(8090)
-
-
-
-console.log('Server running at http://127.0.0.1:8090/');
